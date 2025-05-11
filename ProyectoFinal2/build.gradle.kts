b8436139df44603a0850db4425a3fd19d0471d00
Andrei <andreiaimar1362@gmail.com>
2025-05-11 18:31:30 +0100

    Configurando las implementaciones y versiones

--- ProyectoFinal2/build.gradle.kts
+++ ProyectoFinal2/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 group = "com.example"
@@ -21,6 +22,27 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    implementation(compose.materialIconsExtended)
+    implementation(compose.foundation)
+
+    // Ktor
+    implementation("io.ktor:ktor-client-core:3.0.3")
+    implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.3")
+    implementation("io.ktor:ktor-client-cio:3.0.3")
+
+    // Voyager
+    implementation("cafe.adriel.voyager:voyager-navigator:${property("voyager.version")}")
+    implementation("cafe.adriel.voyager:voyager-transitions:${property("voyager.version")}")
+    implementation("cafe.adriel.voyager:voyager-koin:${property("voyager.version")}")
+    implementation("androidx.annotation:annotation-jvm:1.9.1")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 compose.desktop {
