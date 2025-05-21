c6ca89a13ae58b0d8e3d5f3b312c89fed846daf0
Andrei <andreiaimar1362@gmail.com>
2025-05-21 18:11:48 +0100

    Añadido screen de destalle de prodcto

---
+++ ProyectoFinal2/src/main/kotlin/screens/ProductoDetalleScreen.kt
@@ -0,0 +1,12 @@
+package screens
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import modelo.Producto
+
+class ProductoDetalleScreen(val producto: Producto): Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
